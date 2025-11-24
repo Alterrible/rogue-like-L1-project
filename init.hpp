5a58592fe64a039819972ea71f27ecf99ed8a8c2
alexandre <olivonalex@outlook.fr>
2025-11-24 14:50:53 +0100

    coord spawn point config joueur

--- init.hpp
+++ init.hpp
@@ -1,7 +1,37 @@
-bool charger_configuration(const string& fichier, Jeu& jeu){
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+bool charger_configuration(const string& fichier, Jeu& jeu){
+    fstream flux;
+
+    flux.open(fichier, ios::in);
+    if (flux.is_open()){
+        cout << flux;
+        while (flux.good()){
+            cout << flux;
+        }
+        flux.close();
+    }
+    else{
+        cout << "Erreur : impossible d'ouvrir " << fichier << endl;
+    }
+    return total;
 }
 
 bool charger_carte(const string& fichier, Jeu& jeu){
-    
+    fstream flux;
+
+    flux.open(fichier, ios::in);
+    if (flux.is_open()){
+        cout << flux;
+        while (flux.good()){
+            cout << flux;
+        }
+        flux.close();
+    }
+    else{
+        cout << "Erreur : impossible d'ouvrir " << fichier << endl;
+    }
+    return total;
 }
