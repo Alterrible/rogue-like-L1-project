6b371b75c88f055d88df395c5eb276d638639a8c
alexandre <olivonalex@outlook.fr>
2025-11-27 22:32:41 +0100

    travail sur init tp 26/11

--- init.hpp
+++ init.hpp
@@ -11,12 +11,25 @@ bool charger_configuration(const string& fichier){
         return false;
     }
 
+    int step = 0;
+// logique : on passe
     string ligne;
     while (getline(flux, ligne)) {
-        for (int c ; c <= ligne.length() ; c++){
-            // ...
+        string mot;
+        for (int i = 0; i <= ligne.size(); ++i) {
+            if (i == ligne.size() || ligne[i] == ' ') {
+                if (!mot.empty()) {
+                    if (mot == '[') step++
+                    if (step == 1){
+                        jeu.
+                    }
+                    mot.clear();
+                }
+            } else {
+                mot += ligne[i];
+            }
         }
-    }
+    }   
 
     flux.close();
     return true;
